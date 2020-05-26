82255f4677dcec55e03f395299be225b5c38e7b5
Henrik <henrik.busch@adidas.com>
2020-05-26 11:11:11 +0200

    version for Raspberry-pi version 1.5

--- getdata.py
+++ getdata.py
@@ -17,28 +17,26 @@ def timestamp():
     return unix
 
 
-while True:
-    unixtime = timestamp()
-    if unixtime % 2 == 0:
-        urls = open("URLS.txt", "r")
-        for line in urls:
-            url = line.strip()
-            if not (os.path.isdir("./rawdata")):
-                os.makedirs("./rawdata")
-            if url.find("index") > 0:
-                stonks = url[url.find("index") + 6:]
-                if not (os.path.isdir(f"./rawdata/raw{stonks}")):
-                    os.makedirs(f"./rawdata/raw{stonks}")
+unixtime = timestamp()
+urls = open("URLS.txt", "r")
+for line in urls:
+    url = line.strip()
+    if not (os.path.isdir("./rawdata")):
+        os.makedirs("./rawdata")
+    if url.find("index") > 0:
+        stonks = url[url.find("index") + 6:]
+        if not (os.path.isdir(f"./rawdata/raw{stonks}")):
+            os.makedirs(f"./rawdata/raw{stonks}")
 
-            elif url.find("aktien") > 0:
-                stonks = url[url.find("aktien") + 7:url.find("-")]
-                if not (os.path.isdir(f"./rawdata/raw{stonks}")):
-                    os.makedirs(f"./rawdata/raw{stonks}")
+    elif url.find("aktien") > 0:
+        stonks = url[url.find("aktien") + 7:url.find("-")]
+        if not (os.path.isdir(f"./rawdata/raw{stonks}")):
+            os.makedirs(f"./rawdata/raw{stonks}")
 
-            if not (os.path.isfile(f"./rawdata/raw{stonks}/{stonks}-{unixtime}.html")):
-                site = str(requests.get(url).content).replace(
-                    '\\t', '').replace('\\n', "").replace('\\r', '')
-                rawdata = open(
-                    f"./rawdata/raw{stonks}/{stonks}-{unixtime}.html", "w")
-                rawdata.write(site)
-                rawdata.close()
+    if not (os.path.isfile(f"./rawdata/raw{stonks}/{stonks}-{unixtime}.html")):
+        site = str(requests.get(url).content).replace(
+            '\\t', '').replace('\\n', "").replace('\\r', '')
+        rawdata = open(
+            f"./rawdata/raw{stonks}/{stonks}-{unixtime}.html", "w")
+        rawdata.write(site)
+        rawdata.close()
